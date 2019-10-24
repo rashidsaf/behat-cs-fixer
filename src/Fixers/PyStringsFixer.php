13505dbcd9f14ffe9ea80c41a69eff9c449afe59
rashid <rashid@healthlabs.com>
2019-10-24 13:28:22 -0500

    Actual fix for PyStrings indention and Table columns split

--- src/Fixers/PyStringsFixer.php
+++ src/Fixers/PyStringsFixer.php
@@ -12,6 +12,9 @@ class PyStringsFixer
     /** @var int Padding value from left */
     private const PADDING = 10;
 
+    /** @var PyStringsDto PyStrings Data Object */
+    private $dto;
+
     /**
      * Reformat the text.
      *
@@ -20,16 +23,32 @@ class PyStringsFixer
      */
     public function run(PyStringsDto $dto): string
     {
+        $this->dto = $dto;
         $block_tag = str_pad(
                 PyStringsDto::KEYWORD,
                 strlen(PyStringsDto::KEYWORD) + self::PADDING,
                 ' ',
                 STR_PAD_LEFT) . PHP_EOL;
         $text = '';
-        foreach ($dto->getContent() as $row_text) {
-            $text .= str_pad($row_text, strlen($row_text) + self::PADDING, ' ', STR_PAD_LEFT);
+        $leftPadding = $this->calculateLeftPadding();
+        foreach ($this->dto->getContent() as $row_text) {
+            $text .= str_pad($row_text, strlen($row_text) + $leftPadding, ' ', STR_PAD_LEFT);
         }
 
         return  $block_tag . $text . $block_tag;
+    }
+
+    /**
+     * Calculate the minimum left padding from the left side.
+     *
+     * @return int
+     */
+    private function calculateLeftPadding(): int
+    {
+        $padding =  min(array_map(function ($value) {
+            return strspn($value, ' ');
+        }, $this->dto->getContent()));
+
+        return $padding > self::PADDING ? 0 : self::PADDING - $padding;
     }
 }
